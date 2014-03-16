350ea112005a97bf89f161af2a5a0304ed521031
Camilo <cbarraza11@gmail.com>
2014-03-16 10:44:06 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Akordian2::Application.config.secret_token = '97a154afb6a5f7c9b311cf7f48a450c9ddcdb36c2d05155a0cfd054a7ab27f1ae8111fb0584275d4700c9f9b1b5c6b837e724a4f0697fac9b37e03623d925d96'
