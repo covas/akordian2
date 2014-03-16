05bf8c293f8d9fe7c7d99403c69a9fad3469ce4a
Camilo <cbarraza11@gmail.com>
2014-03-16 11:06:28 -0500

    le agregue test con acceso a neo4j y consulta de todos los nodos

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,7 @@ module Akordian2
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
@@ -65,5 +66,6 @@ module Akordian2
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
+    config.gem "httparty"
   end
 end
